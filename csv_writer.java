aa8db9f4d750071e045a946ae1b4e33456dd2fc5
T Sri Skanthan <sriskanthan2006@gmail.com>
2026-02-07 14:07:48 +0530

    Update csv_writer.java
    not fully done, still figuring out..

---
+++ csv_writer.java
@@ -0,0 +1,8 @@
+public class csv_writer {
+    public static void main(String[] args) {
+        // This method can be used to write data to the CSV file if needed
+        // For now, it can return the existing data read from the CSV file
+        String csvFile = "main_dataset.csv";
+
+    }
+}
